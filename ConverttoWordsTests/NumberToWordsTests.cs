cf5a9bb8d8fb13a2924348a5f8d5cd79481c1322
Bala <baprofessional@gmail.com>
2020-06-04 21:45:12 +0530

    Add project files.

---
+++ ConverttoWordsTests/NumberToWordsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConverttoWords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverttoWords.Tests
+{
+    [TestClass()]
+    public class NumberToWordsTests
+    {
+        [TestMethod()]
+        public void NumberCovertionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
